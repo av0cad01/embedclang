7bdf0ba412c6fc7902225c59c9f746b7b637bb20
Artyom Ermokhin <artyomermokhin@MacBook-Pro-Artyom.local>
2023-10-28 12:51:36 +0300

    Correction honeycomb.c file

--- HW1/honeycomb.c
+++ HW1/honeycomb.c
@@ -1,8 +1,6 @@
 /*
  * honeycomb.c
  * 
- * Copyright 2023 Artyom Ermokhin <artyomermokhin@MacBook-Pro-Artyom.local>
- * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
